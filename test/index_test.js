91b128f9e20c990ff76205c0b7e8a14596aea69d
Artem Shitov <inbox@artemshitov.ru>
2014-02-25 03:40:26 +0400

    Tests

---
+++ test/index_test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var non    = require('./../index.js');
+
+var object = {
+  a: {
+    b: {
+      c: 1
+    }
+  }
+};
+
+assert.deepEqual(non(object, 'a'), {b: {c: 1}});
+assert.deepEqual(non(object, 'a', 'b'), {c: 1});
+assert.strictEqual(non(object, 'a', 'b', 'c'), 1);
+
+assert.strictEqual(non(object, 'a', 'b', 'a'), void 0);
+assert.strictEqual(non(object, 'a', 'a'), void 0);
