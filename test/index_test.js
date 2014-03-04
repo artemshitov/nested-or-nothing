af59e292bd27315cd82a37f1db4190dc96ae85cb
Artem Shitov <artemshitov@yandex-team.ru>
2014-03-04 18:01:45 +0400

    Test null support

--- test/index_test.js
+++ test/index_test.js
@@ -4,16 +4,19 @@ var non    = require('./../index.js');
 var object = {
   a: {
     b: {
-      c: 1
+      c: 1,
+      d: null
     }
   }
 };
 
-assert.deepEqual(non(object, 'a'), {b: {c: 1}});
-assert.deepEqual(non(object, 'a', 'b'), {c: 1});
+assert.deepEqual(non(object, 'a'), {b: {c: 1, d: null}});
+assert.deepEqual(non(object, 'a', 'b'), {c: 1, d: null});
 assert.deepEqual(non(object), object);
 assert.strictEqual(non(object, 'a', 'b', 'c'), 1);
 
 assert.strictEqual(non(object, 'a', 'b', 'a'), void 0);
+assert.strictEqual(non(object, 'a', 'b', 'd'), void 0);
+assert.strictEqual(non(object, 'a', 'b', 'd', 'e'), void 0);
 assert.strictEqual(non(object, 'a', 'a'), void 0);
 assert.strictEqual(non(), void 0);
